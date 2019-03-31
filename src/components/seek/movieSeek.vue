67115b1e9c197574c7f602841078ce8475cae4e0
项宗捷 <moonrbt@sina.com>
2019-03-31 09:49:55 +0800

    seek页面的完善

---
+++ src/components/seek/movieSeek.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="box">
+    <mt-search
+      v-model="value"
+      cancel-text="取消"
+      placeholder="搜索">
+    </mt-search>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Search } from 'mint-ui'
+Vue.component(Search.name, Search)
+export default {
+  data () {
+    return{
+      value:'搜电影 、搜影院'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .box{
+    width: 100%;
+    position: fixed;
+    top: 16vw;
+    z-index: 101;
+  }
+</style>
--- src/views/seek.vue
+++ src/views/seek.vue
@@ -1,14 +1,43 @@
 <template>
-  <h1>搜索</h1>
+  <div>
+    <h1 @click="backor">{{back}}</h1>
+    <movie-header></movie-header>
+    <movie-seek></movie-seek>
+  </div>
 </template>
 
 <script>
+import movieSeek from '../components/seek/movieSeek'
+import movieHeader from '../components/header'
 export default {
-
+  data () {
+    return {
+      back: '<'
+    }
+  },
+  components: {
+    movieSeek,
+    movieHeader
+  },
+  methods: {
+    backor () {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
+
 <style lang="scss" scoped>
   h1{
-    color: fuchsia;
+    width: 10vw;
+    height: 10vw;
+    font-size: 7vw;
+    line-height: 10vw;
+    text-align: center;
+    color: white;
+    z-index: 103;
+    position: fixed;
+    top: 3vw;
+    left:3vw;
   }
 </style>
